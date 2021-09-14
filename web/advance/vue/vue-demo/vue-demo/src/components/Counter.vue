b5f151a731f40f9fad546cfdd5c8ef16fc38d0c1
chenminghong <chenmh@bell.ai>
2021-09-14 18:31:29 +0800

    vue cli创建项目

---
+++ web/advance/vue/vue-demo/vue-demo/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>Counter: {{ counter }}-{{message}}</div>
+</template>
+
+<script>
+export default {
+    name: "Counter",
+    props: {
+        counter: Number
+    },
+    data: function() {
+        return {
+            message: this.counter || 'hello world'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/advance/vue/vue-demo/vue-demo/src/components/Tester.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Tester: <Counter /></div>
+</template>
+
+<script>
+import Counter from './Counter.vue'
+
+export default {
+    name: "Tester",
+    components: {
+        Counter
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
